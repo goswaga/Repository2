2a735012a323cd32493d10ae3e69dc5cc4c16dc2
Ankita <Ankita@LAPTOP-T00VOURR>
2020-01-09 09:51:04 +0530

    pushing

---
+++ Project_1/src/pkg1/Class1.java
@@ -0,0 +1,19 @@
+ package pkg1;
+
+public class Class1
+{
+int a; 
+
+public void abc()
+{ System.out.println("Welcome");
+}
+
+public static void main(String[] args) 
+{
+	Class1 deepak=new Class1();
+	deepak.abc();
+	deepak.a=35;
+	System.out.println(deepak.a);
+} }
+
+ 
